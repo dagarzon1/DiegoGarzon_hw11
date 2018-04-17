836ec06dae50df4983ef32295f4d8c539ae2b225
Diego Garzon <da.garzon1@uniandes.edu.co>
2018-04-17 16:58:09 -0500

    comments

--- DG_graph.py
+++ DG_graph.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Obtener los datos
 data=np.loadtxt("tray.txt")
 
+#Graficar datos de posicion vs tiempo
 plt.scatter(data[:,2],data[:,0], label="x vs t")
 plt.xlabel("tiempo(s)")
 plt.ylabel("posicion(m)")
@@ -11,6 +13,7 @@ plt.legend()
 plt.savefig("pos.png")
 plt.clf()
 
+#Graficar datos de velocidad vs tiempo
 plt.scatter(data[:,2],data[:,1], label="v vs t")
 plt.xlabel("tiempo(s)")
 plt.ylabel("velocidad(m/s)")
@@ -19,6 +22,7 @@ plt.legend()
 plt.savefig("vel.png")
 plt.clf()
 
+#Graficar datos de velocidad vs posicion
 plt.scatter(data[:,0],data[:,1], label="v vs x")
 plt.xlabel("posicion(m)")
 plt.ylabel("velocidad(m/s)")
